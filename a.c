55b11d4887c79e1c5a2fec12db5ffa1d95e7afea
858509143 <858509143@qq.com>
2017-03-26 21:08:07 +0800

    乘法表.c

---
+++ a.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# define MAX a*b
+int main(void)
+{
+	int a, b;
+	a = 1,b = 0;
+	while (a <= 9)
+	{
+	while (b <= 9)
+	{
+		printf("%d*%d=%2d ", a, b, MAX);
+		b++;
+	}
+	printf("\n");
+	a++;
+	b = a;
+	}
